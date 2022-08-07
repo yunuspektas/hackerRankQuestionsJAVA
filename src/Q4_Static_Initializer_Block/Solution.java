9b6b00d7afecd7aae6bcf2f74ace765f51336279
Yunus Pektas <yunuspektas@protonmail.com>
2022-08-07 18:58:46 +0300

    static initializer block

---
+++ src/Q4_Static_Initializer_Block/Solution.java
@@ -0,0 +1,20 @@
+package Q4_Static_Initializer_Block;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        int h = sc.nextInt();
+        if(b<=0 || h<=0) {
+            System.out.println( "java.lang.Exception: Breadth and height must be positive" );
+        }else {
+            System.out.println(b*h);
+        }
+
+
+    }
+}

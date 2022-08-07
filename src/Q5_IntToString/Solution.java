d0e476fbaf7c103f9b0e4a73d1dcd3d39bd70dd1
Yunus Pektas <yunuspektas@protonmail.com>
2022-08-07 19:06:31 +0300

    Int to String

---
+++ src/Q5_IntToString/Solution.java
@@ -0,0 +1,14 @@
+package Q5_IntToString;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = Integer.toString(sc.nextInt()) ;
+        System.out.println("Good job");
+
+
+    }
+}
